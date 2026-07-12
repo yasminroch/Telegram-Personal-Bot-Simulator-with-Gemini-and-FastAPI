a51491013304b1930396c6a38678efb6d59aa7b6
yasminroch <yasmin.jesus@sou.inteli.edu.br>
2026-07-12 20:38:02 -0300

    adicionando prints para rastreio de erros em logs

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,6 +10,8 @@ chat_service = ChatService()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
+print("TOKEN EXISTE:", TELEGRAM_TOKEN is not None)
+
 
 @router.post("/webhook")
 async def webhook(request: Request):
@@ -21,21 +23,30 @@ async def webhook(request: Request):
     mensagem = data.get("message", {}).get("text", "")
     chat_id = data.get("message", {}).get("chat", {}).get("id")
 
+    print("Mensagem recebida:", mensagem)
+    print("Chat ID:", chat_id)
+
     if not mensagem or not chat_id:
+        print("Mensagem ou chat_id vazio")
         return {"ok": True}
 
     respostas = chat_service.gerar_resposta(mensagem)
+
+    print("Respostas geradas:", respostas)
 
     async with httpx.AsyncClient() as client:
         for r in respostas:
             await asyncio.sleep(calcular_delay(r))
 
-            await client.post(
+            response = await client.post(
                 f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
                 json={
                     "chat_id": chat_id,
                     "text": r
                 }
             )
+
+            print("Status Telegram:", response.status_code)
+            print("Resposta Telegram:", response.text)
 
     return {"ok": True}
