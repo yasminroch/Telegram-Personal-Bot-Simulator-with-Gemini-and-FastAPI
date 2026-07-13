eea5c83fff53bf9edab613fd924c303725208880
yasminroch <yasmin.jesus@sou.inteli.edu.br>
2026-07-12 21:38:10 -0300

    mudança estrutura sdk nova biblioteca gemini

--- app/infrastructure/ai/gemini_client.py
+++ app/infrastructure/ai/gemini_client.py
@@ -1,21 +1,29 @@
 from google import genai
+from google.genai import types
+
 from app.core.config import GEMINI_API_KEY
 
-genai.configure(api_key=GEMINI_API_KEY)
 
 class GeminiClient:
+
     def __init__(self, system_prompt: str):
-        self.model = genai.GenerativeModel(
-            model_name="models/gemini-2.5-flash",
-            system_instruction=system_prompt,
+        self.client = genai.Client(
+            api_key=GEMINI_API_KEY
         )
 
+        self.system_prompt = system_prompt
+
+
     def generate(self, contents):
-        response = self.model.generate_content(
-            contents,
-            generation_config=genai.types.GenerationConfig(
+
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=contents,
+            config=types.GenerateContentConfig(
+                system_instruction=self.system_prompt,
                 temperature=0.9,
                 max_output_tokens=600,
             )
         )
+
         return response.text.strip()
